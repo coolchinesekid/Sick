69a6c376b4ce538f31abf1733967469bbfbf7f39
Ethan Ng <ethan_ng_224@yahoo.com>
2020-01-15 20:10:53 -0800

    Initial Commit

---
+++ Sick/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Sick
+//
+//  Created by Ethan Ng on 1/15/20.
+//  Copyright © 2020 Ethan Ng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
